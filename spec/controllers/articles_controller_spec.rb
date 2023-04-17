676e05d89b112506749a26e3227473be3fc2f58d
Carlos Garibay <carlos.garibay@tec.mx>
2023-04-17 07:45:59 -0600

    adds expects to articles

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe ArticlesController, type: :controller do
 
   before { subject }
 
-  shared_examples_for 'render template' do
+  shared_examples_for 'articles controller actions' do
+    it 'assigns instance variable' do
+      expect(assigned_var).to eq(expected_value)
+    end
+
     it 'renders the expected template' do
       expect(response).to render_template(template)
     end
@@ -14,22 +18,58 @@ RSpec.describe ArticlesController, type: :controller do
   describe "GET index" do
     subject { get :index }
     let(:template) { "index" }
+    let(:assigned_var) { assigns(:articles) }
+    let(:expected_value) { [article] }
 
-    it "assigns @articles" do
-      expect(assigns(:articles)).to eq([article])
-    end
-
-    it_should_behave_like 'render template'
+    it_should_behave_like 'articles controller actions'
   end
 
   describe "GET show" do
     subject { get :show, params: { id: article.id } }
     let(:template) { "show" }
+    let(:assigned_var) { assigns(:article) }
+    let(:expected_value) { article }
 
-    it "assigns @article" do
-      expect(assigns(:article)).to eq(article)
+    it_should_behave_like 'articles controller actions'
+  end
+
+  describe "POST create" do
+    context 'when article fails body validation' do
+      subject { post :create, params: { article: { title: 'A test', body: 'invalid' } } }
+
+      it 'should render new view' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'should have unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
 
-    it_should_behave_like 'render template'
+    context 'when article fails title validation' do
+      subject { post :create, params: { article: { body: 'this is a valid body' } } }
+
+      it 'should render new view' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'should have unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'when article is saved successfully' do
+      subject { post :create, params: { article: { title: 'A title', body: 'this is a valid body' } } }
+
+      it 'should redirect to show path' do
+        created_article = Article.find_by(title: 'A title', body: 'this is a valid body')
+        expect(response).to redirect_to("/articles/#{created_article.id}")
+      end
+
+      it 'should create a new article' do
+        created_article = Article.find_by(title: 'A title', body: 'this is a valid body')
+        expect(created_article).not_to be_nil
+      end
+    end
   end
 end
